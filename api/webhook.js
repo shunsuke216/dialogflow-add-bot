62243ce58fb3add6a88da3488cd735c5868ff546
shunsuke216 <a.katsukibang@gmail.com>
2026-01-28 14:52:40 +0800

    Refactor webhook handler to use Express.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,23 +1,52 @@
-export default function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).send('Method Not Allowed');
-  }
+const express = require('express');
+const app = express();
+app.use(express.json());
 
-  const params = req.body.queryResult.parameters;
+app.post('/', (req, res) => {
+    // 取得 Dialogflow 傳過來的參數
+    const { parameters } = req.body.queryResult;
+    const num1 = parseFloat(parameters['number']);    // 第一個數字
+    const num2 = parseFloat(parameters['number1']);   // 第二個數字
+    const operator = parameters['operator'];          // 運算符號 (加、減、乘、除)
 
-  const a = Number(params.number1);
-  const b = Number(params.number2);
+    let result = 0;
+    let message = "";
 
-  if (isNaN(a) || isNaN(b)) {
-    return res.json({
-      fulfillmentText: '我需要兩個數字才能幫你加喔～'
+    // 邏輯判斷
+    switch (operator) {
+        case '加':
+        case '+':
+            result = num1 + num2;
+            break;
+        case '減':
+        case '-':
+            result = num1 - num2;
+            break;
+        case '乘':
+        case '*':
+        case 'x':
+            result = num1 * num2;
+            break;
+        case '除':
+        case '/':
+            if (num2 === 0) {
+                message = "數學老師說不能除以 0 喔！";
+            } else {
+                result = num1 / num2;
+            }
+            break;
+        default:
+            message = "我不確定你想做什麼運算，請試試看『5 乘以 8』";
+    }
+
+    if (!message) {
+        message = `計算結果是：${result}`;
+    }
+
+    // 回傳給 Dialogflow 的格式
+    res.json({
+        fulfillmentText: message
     });
-  }
+});
 
-  const result = a + b;
-
-  res.json({
-    fulfillmentText: `${a} + ${b} = ${result}`
-  });
-}
-
+module.exports = app;
